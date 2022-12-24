02c5ab5e04d71f334ff66f43aeb4dfb0edf8e74a
aashutosh148 <aashutoshsingh468847@gmail.com>
2022-12-24 21:49:03 +0530

    adding file

---
+++ recoganise_speech.py
@@ -0,0 +1,103 @@
+import requests
+import pyttsx3
+import speech_recognition as sr
+from decouple import config
+from datetime import datetime
+from random import choice
+from utils import opening_text
+from pprint import pprint
+import openai
+from gtts import gTTS
+
+
+openai.api_key = "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"
+
+USERNAME = "aashutosh"
+BOTNAME = "SAM!"
+
+
+engine = pyttsx3.init('sapi5')
+
+# Set Rate
+engine.setProperty('rate', 190)
+
+# Set Volume
+engine.setProperty('volume', 1.0)
+
+# Set Voice (Female)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+# Text to Speech Conversion
+def speak(text):
+    """Used to speak whatever text is passed to it"""
+
+    engine.say(text)
+    engine.runAndWait()
+
+
+# Greet the user
+def greet_user():
+    """Greets the user according to the time"""
+    
+    hour = datetime.now().hour
+    if (hour >= 6) and (hour < 12):
+        speak(f"Good Morning {USERNAME}")
+    elif (hour >= 12) and (hour < 16):
+        speak(f"Good afternoon {USERNAME}")
+    elif (hour >= 16) and (hour < 19):
+        speak(f"Good Evening {USERNAME}")
+    speak(f"I am {BOTNAME}. How may I assist you?")
+
+
+# Takes Input from User
+def take_user_input():
+    """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
+    
+    r = sr.Recognizer()
+    with sr.Microphone(device_index=2) as source:
+        print('Listening....')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        if not 'exit' in query or 'stop' in query:
+            speak(choice(opening_text))
+        else:
+            hour = datetime.now().hour
+            if hour >= 21 and hour < 6:
+                speak("Good night sir, take care!")
+            else:
+                speak('Have a good day sir!')
+            exit()
+    except Exception:
+        speak('Sorry, I could not understand. Could you please say that again?')
+        query = 'None'
+    return query
+
+
+
+def generate_text(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    return message
+
+
+greet_user()
+
+while True:
+    
+    input=take_user_input()
+    output=(generate_text(input))
+    speak(output)    
